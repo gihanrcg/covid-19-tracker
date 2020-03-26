bf7df99965065ef135d636d95cfe4247629326b7
= <=>
2020-03-26 21:20:25 +0530

    Added new datasource to graph

--- src/Graphs/GraphComp.jsx
+++ src/Graphs/GraphComp.jsx
@@ -12,27 +12,18 @@ class GraphComp extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            countryDetails: null,
-            countries: [],
+            countries: null,
             countryNames: [],
             selectedCountry: null,
-            countryCode: null,
-            labelsCases: [],
-            dataCases: [],
-            labelsDeaths: [],
-            dataDeaths: [],
-            labelsRecover: [],
-            dataRecover: [],
-
         }
     }
 
     componentDidMount() {
-        fetch('https://corona.lmao.ninja/historical')
+        fetch('https://pomber.github.io/covid19/timeseries.json')
             .then(response => response.json())
             .then(response => {
 
-                if (response.length > 0) {
+                if (response) {
                     this.setState({
                         countries: response
                     })
@@ -45,104 +36,40 @@ class GraphComp extends Component {
 
 
     setData = (country) => {
-        this.setCases(country);
-        this.setDeaths(country);
-        this.setRecoveries(country);
-    }
 
+        let labels = [];
+        let deaths = [];
+        let recovered = [];
+        let cases = [];
 
-    setRecoveries = (country) => {
-        if (country) {
-            let timeLineData = country.timeline;
-            let p = timeLineData.recovered;
-            let la = [];
-            let da = []
-            for (var key in p) {
-                if (p.hasOwnProperty(key)) {
-                    la.push(key);
-                    da.push(p[key])
-                }
-            }
-            this.setState({
-                labelsRecover: la,
-                dataRecover: da
-            })
-        }
-    }
-    setDeaths = (country) => {
-        if (country) {
-            let timeLineData = country.timeline;
-            let p = timeLineData.deaths;
-            let la = [];
-            let da = []
-            for (var key in p) {
-                if (p.hasOwnProperty(key)) {
-                    la.push(key);
-                    da.push(p[key])
-                }
-            }
-            this.setState({
-                labelsDeaths: la,
-                dataDeaths: da
-            })
-        }
-    }
+        console.log('con', country)
 
-    setCases = (country) => {
-        if (country) {
-            let timeLineData = country.timeline;
-            let p = timeLineData.cases;
-            let la = [];
-            let da = []
-            for (var key in p) {
-                if (p.hasOwnProperty(key)) {
-                    la.push(key);
-                    da.push(p[key])
-                }
-            }
+        country.map((con) => {
+            labels.push(con.date);
+            deaths.push(con.deaths);
+            recovered.push(con.recovered);
+            cases.push(con.confirmed);
+        })
 
-            this.setState({
-                labelsCases: la,
-                dataCases: da
-            })
-        }
+        this.setState({
+            labels: labels,
+            deaths: deaths,
+            recovered: recovered,
+            cases: cases
+        })
     }
 
     getCountryValues = (country) => {
-
-        fetch(`https://corona.lmao.ninja/historical/${country.value}`)
-            .then(response => response.json())
-            .then(response => {
-
-                if (response) {
-                    this.setState({
-                        countryDetails: response
-                    })
-                    this.setData(response);
-                }
-            })
-            .catch((error) => console.log('Something went wrong...', error));
-        return null;
+        this.setState({
+            selectedCountry: country.value
+        })
+        this.setData(this.state.countries[country.value]);
     }
 
     selectChangeHandle = (val) => {
 
         this.getCountryValues(val);
-        this.setState({
-            selectedCountry : val.value
-        })
-
-        // for (var i = 0; i < this.state.countries.length; i++) {
-        //     if (this.state.countries[i].country === val.value) {
-        //         this.setState({
-        //             selectedCountry: this.state.countries[i],
-
-        //         })
-        //         this.setData(this.state.countries[i]);
-
-        //         this.fetchCountryCode(i);
-        //     }
-        // }
+        
     }
 
     fetchCountryCode = (i) => {
@@ -157,14 +84,14 @@ class GraphComp extends Component {
     }
 
     setCountryNames = () => {
-        console.log('con',this.state)
+
         const countryNames = [];
-        for (let i = 0; i < this.state.countries.length; i++) {
-            
-            if (!countryNames.includes(this.state.countries[i].country)) {
+        var p = this.state.countries;
+        for (var key in p) {
+            if (p.hasOwnProperty(key)) {
                 countryNames.push({
-                    label: this.state.countries[i].country,
-                    value: this.state.countries[i].country
+                    label: key,
+                    value: key
                 });
             }
         }
@@ -192,17 +119,17 @@ class GraphComp extends Component {
                 <Select options={this.state.countryNames}
                     onChange={opt => this.selectChangeHandle(opt)} />
 
- 
+
                 {
 
                     (this.state.selectedCountry != null) && (
                         <div>
                             <br />
-                            <GraphOfCases countryCode={this.state.countryCode} labels={this.state.labelsCases} cases={this.state.dataCases} />
+                            <GraphOfCases countryCode={this.state.countryCode} labels={this.state.labels} cases={this.state.cases} />
                             <Paper style={{ marginTop: 20 }} />
-                            <GraphOfDeaths countryCode={this.state.countryCode} labels={this.state.labelsDeaths} cases={this.state.dataDeaths} />
+                            <GraphOfDeaths countryCode={this.state.countryCode} labels={this.state.labels} cases={this.state.deaths} />
                             <Paper style={{ marginTop: 20 }} />
-                            <GraphOfRecover countryCode={this.state.countryCode} labels={this.state.labelsRecover} cases={this.state.dataRecover} />
+                            <GraphOfRecover countryCode={this.state.countryCode} labels={this.state.labels} cases={this.state.recovered} />
                         </div>
                     )
                 }
